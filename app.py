8ca5a084c61b8b0f22a00378ab8f170a80dd4cfa
ai-mayurpatil <ai.mayurpatil@gmail.com>
2024-07-13 19:53:57 +0530

    display text done

--- app.py
+++ app.py
@@ -5,3 +5,22 @@ import numpy as np
 # Title of the app
 st.title("Hi Streamlit")
 
+#Text
+st.text("Hello World")
+
+name = "Mayur"
+st.text("This is {}".format(name))
+
+# Header
+st.header("This is a header")
+
+# Subheader
+st.subheader("This is a subheader")
+
+# Title
+st.title("This is a title")
+
+#  Markdown
+st.markdown("This is markdown")
+st.markdown("# This is markdown")
+st.markdown("## This is markdown")
